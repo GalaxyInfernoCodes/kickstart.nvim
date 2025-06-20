7280bd56aac104c9f6c8ad38e99a8e3bd34d2b72
Sarah Glasmacher <galaxyinferno.codes@gmail.com>
2025-06-20 18:52:15 +0200

    added window title

--- init.lua
+++ init.lua
@@ -104,6 +104,8 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 -- vim.o.relativenumber = true
 
+vim.o.title = true
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
